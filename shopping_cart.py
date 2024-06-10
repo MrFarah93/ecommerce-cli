4fda659f51b0ba94cf95d0fff929e0a2c7ae1072
achoclate <nyabokeann99@gmail.com>
2024-06-10 10:33:00 +0300

    write a functionality for adding to cart

--- shopping_cart.py
+++ shopping_cart.py
@@ -2,3 +2,9 @@ class ShoppingCart:
     def __init__(self):
         self.cart = {}  # Initialize an empty cart
         self.product_prices = {'123': 10.0, '456': 20.0}  # Example prices
+
+    def add_to_cart(self, product_id, quantity):
+        if product_id in self.cart:
+            self.cart[product_id] += quantity
+        else:
+            self.cart[product_id] = quantity
